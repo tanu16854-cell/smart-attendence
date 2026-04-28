6ba11b1d516b27ec1e75edd15ebd58ac8250e71c
Tanu Rana <tanu16854@gmail.com>
2026-04-28 23:13:05 +0530

    update

--- db.js
+++ db.js
@@ -1,19 +1,14 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-});
-
-db.connect((err) => {
-    if (err) {
-        console.log("❌ DB Error:", err.message);
-    } else {
-        console.log("✅ MySQL Connected");
-    }
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = db;
